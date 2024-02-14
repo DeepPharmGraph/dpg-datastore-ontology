880330cbb229fb97d73689c6d9f749f0f2108dc0
Brian Lu <me@greencappuccino.net>
2024-02-14 05:01:58 -0500

    Load ontologies into the default graph

--- src/00-init-load.sh
+++ src/00-init-load.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-oxigraph load --file "$ONTOLOGY_DIR"/foaf.rdf --graph http://xmlns.com/foaf/0.1/ "$@"
-oxigraph load --file "$ONTOLOGY_DIR"/vcard.ttl --graph http://www.w3.org/2006/vcard/ns "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/foaf.rdf "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/vcard.ttl "$@"
