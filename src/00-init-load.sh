29cec2c01f8ec6f7875d06647b903a28f69ae28f
Brian Lu <me@greencappuccino.net>
2024-02-14 05:58:53 -0500

    Add Pattern

--- src/00-init-load.sh
+++ src/00-init-load.sh
@@ -5,6 +5,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-oxigraph load --file "$ONTOLOGY_DIR"/foaf.rdf "$@"
-oxigraph load --file "$ONTOLOGY_DIR"/vcard.ttl "$@"
-oxigraph load --file "$ONTOLOGY_DIR"/prov-o.ttl "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/foaf.rdf --format "application/rdf+xml" "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/vcard.ttl --format "text/turtle" "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/prov-o.ttl --format "text/turtle" "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/pattern.owl --format "application/rdf+xml" "$@"
