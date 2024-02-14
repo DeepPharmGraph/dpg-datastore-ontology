9c7ee58030c91d38bfe380a07df29d5e4eb6c034
Brian Lu <me@greencappuccino.net>
2024-02-13 20:42:44 -0500

    Load vCard ontology

--- init-load.sh
+++ init-load.sh
@@ -6,3 +6,4 @@ set -o nounset
 set -o xtrace
 
 oxigraph load --file "$ONTOLOGY_DIR"/foaf.rdf --graph http://xmlns.com/foaf/0.1/ "$@"
+oxigraph load --file "$ONTOLOGY_DIR"/vcard.ttl --graph http://www.w3.org/2006/vcard/ns "$@"
